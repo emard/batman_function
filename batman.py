6f79e9afa866319e8b17a412a2be438bba34a251
emard <vordah@gmail.com>
2020-06-08 13:40:29 +0200

    cleanup

--- batman.py
+++ batman.py
@@ -15,7 +15,6 @@ from matplotlib import pyplot as plt
 from math import fabs,sqrt
 sampling_interval=0.2
 fpath_xpts = np.arange(-7.0, 7.0, sampling_interval)
-#fpath_ytop = np.array([fabs(x/2)-(3*sqrt(33)-112)/7*x**2+sqrt(1-(fabs(fabs(x)-2)-1)**2)-3 for x in fpath_xpts])
 fpath_ytop = np.array([batman_top(x) for x in fpath_xpts])
 fpath_ybot = np.array([batman_bot(x) for x in fpath_xpts])
 plt.plot(
