e5ffd5636426c8b3f5180e1b99fecfbd189f431a
emard <vordah@gmail.com>
2020-06-08 13:22:57 +0200

    numeric map function

--- batman.py
+++ batman.py
@@ -4,9 +4,10 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 sampling_interval=0.2
 fpath_xpts = np.arange(-7.0, 7.0, sampling_interval)
-fpath_ytop = fpath_xpts
+fpath_ytop = np.array([math.fabs(x/2) for x in fpath_xpts])
 fpath_ybot = fpath_xpts
 plt.plot(
       fpath_xpts, 1000*fpath_ytop, "r-",
