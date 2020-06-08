c52acf8162244c27b0b1d91f923bfdb337a826e7
emard <vordah@gmail.com>
2020-06-08 13:52:42 +0200

    cleanup

--- batman.py
+++ batman.py
@@ -3,8 +3,10 @@
 # original: https://www.desmos.com/calculator/dnzfajfpym
 
 def batman_top(x):
-  if x > -4 and x < 4:
+  if fabs(x) <= 4:
     return fabs(x/2)-(3*sqrt(33)-7)/112*x**2+sqrt(1-(fabs(fabs(x)-2)-1)**2)-3
+  if fabs(x) >= 4 and fabs(x) <= 7:
+    return -3*sqrt(-(x/7)**2+1)
   return 0
 
 def batman_bot(x):
@@ -13,14 +15,13 @@ def batman_bot(x):
 import numpy as np
 from matplotlib import pyplot as plt
 from math import fabs,sqrt
-sampling_interval=0.2
-fpath_xpts = np.arange(-7.0, 7.0, sampling_interval)
+sampling_interval=0.1
+fpath_xpts = np.arange(-7.0, 7.0+sampling_interval, sampling_interval)
 fpath_ytop = np.array([batman_top(x) for x in fpath_xpts])
 fpath_ybot = np.array([batman_bot(x) for x in fpath_xpts])
 plt.plot(
       fpath_xpts, fpath_ytop, "r-",
       fpath_xpts, fpath_ybot, "b-"
 )
-plt.ylabel('mm blue:true red:rover')
 plt.show()
     
