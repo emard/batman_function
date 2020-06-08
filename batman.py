832f6a2a4f01ed900f44261739fecad4ba0918f4
emard <vordah@gmail.com>
2020-06-08 13:17:56 +0200

    proper range

--- batman.py
+++ batman.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
+
+# original: https://www.desmos.com/calculator/dnzfajfpym
+
 import numpy as np
 from matplotlib import pyplot as plt
-length=10
 sampling_interval=0.2
-fpath_xpts = np.arange(0.0, length, sampling_interval)
+fpath_xpts = np.arange(-7.0, 7.0, sampling_interval)
 fpath_ytop = fpath_xpts
 fpath_ybot = fpath_xpts
 plt.plot(
-      fpath_xpts, 1000*fpath_ytop, "b-",
-      fpath_xpts,-1000*fpath_ybot, "r-"
+      fpath_xpts, 1000*fpath_ytop, "r-",
+      fpath_xpts,-1000*fpath_ybot, "b-"
 )
 plt.ylabel('mm blue:true red:rover')
 plt.show()
